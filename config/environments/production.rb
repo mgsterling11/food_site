c37192c3d500d61c6bdbfbdd82ab7306fea57d49
mgsterling11 <m.g.sterling11@gmail.com>
2015-12-11 11:53:09 -0500

    updates production email host to get-cookin.herokuapp.com for email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -10,7 +10,7 @@ Rails.application.configure do
   # Rake tasks automatically ignore this option for performance.
   config.eager_load = true
 
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: "get-cookin.herokuapp.com/" }
     config.action_mailer.delivery_method = :smtp
     config.action_mailer.raise_delivery_errors = true
     config.action_mailer.smtp_settings =  {
