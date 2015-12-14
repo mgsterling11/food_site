79f13abfb203409719b25f0f34847140ddd83848
mgsterling11 <m.g.sterling11@gmail.com>
2015-12-14 10:09:10 -0500

    quick updates to shopping list view objects

--- app/view_objects/shopping_list_view_object.rb
+++ app/view_objects/shopping_list_view_object.rb
@@ -2,8 +2,10 @@ class ShoppingListViewObject
   attr_reader :proportions, :recipe
 
   def initialize(recipe, proportions)
-    @recipe = recipe
-    @proportions = proportions
+    # @recipe = recipe
+    # @proportions = proportions
+    @ingredients = list_ingredients_and_quantities(proportions)
+    @units = proportions
   end
 
   def list_ingredients_and_quantities
@@ -41,5 +43,35 @@ class ShoppingListViewObject
 
   def categories_class
     recipe.categories.map(&:id).join(' ')
+  end
+
+
+  def unit
+    if proportion.unit
+      @unit = proportion.unit.name
+    else
+      @unit = ""
+    end
+  end
+
+  def quantity_conversion(quantity)
+    conversions = { 0.125 => "1/8", 0.25 => "1/4", 0.333 => "1/3", 0.667 => "2/3", 0.5 => "1/2", 0.75 => "3/4" }
+    if quantity == 0.0
+      ""
+    elsif quantity.to_i == quantity
+      quantity.to_i
+    elsif conversions.keys.include?(quantity)
+      conversions[quantity]
+    elsif quantity
+      quantity.round(3)
+    end   
+  end
+
+  def quantity
+    quantity_conversion(original_quantity)
+  end
+
+  def display
+    "#{quantity} #{unit} #{ingredient}"
   end
 end
